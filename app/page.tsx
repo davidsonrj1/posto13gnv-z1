a0e5f44a8cbdd5a79d21816474e590195c631bf5
Davidson Linhares <115379178+davidsonrj1@users.noreply.github.com>
2025-07-02 22:52:44 -0300

    atualização 3

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
                   priority
                 />
               </div>
-              // Whatsapp Mudança
+              
               <Button
                 className="bg-green-600 hover:bg-green-700 text-white p-2.5 rounded-full shadow-lg hover:shadow-xl transition-all duration-300"
                 onClick={() =>
